9fd87bbe0b0f8af1ea187d4f56a871dd4d113c02
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-07-12 10:00:43 -0300

    feat: Criado a migration usuario, iserindo created_at e updated_at

--- src/database/migrations/20230712121117-create-table-usuarios.js
+++ src/database/migrations/20230712121117-create-table-usuarios.js
@@ -49,6 +49,14 @@ module.exports = {
       type:Sequelize.ENUM('Ativo', 'Inativo'),
       allowNull:false,
       defaultValue: 'Ativo'
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
     }
   }); 
   },
