572473e650910776d5e06ea8253a2c4f7edf36f4
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-07-12 08:57:17 -0300

    feat: Adiçao arquivo routes

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const PORT = 3333
-const { connection } = require('./database/connection')
+const routes = require('./routes/routes')
 
-
+require('./database/connection')
 
 
 app.use(express.json())
+app.use(routes)
 app.use(cors())
+
 app.listen(PORT, () => console.log(`Servidor executando  em http://localhost:${PORT}`))
 
