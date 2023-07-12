0e269888b51cbcac7fb5fb43e5c47781943296ce
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-07-12 10:21:39 -0300

    feat: Criado a migration usuarios , trocando users por usuarios

--- src/database/migrations/20230712121117-create-table-usuarios.js
+++ src/database/migrations/20230712121117-create-table-usuarios.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-   await queryInterface.createTable('users', { 
+   await queryInterface.createTable('usuarios', { 
     id: {
       type:Sequelize.INTEGER,
       allowNull: false,
@@ -66,7 +66,7 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-   await queryInterface.dropTable('users');
+   await queryInterface.dropTable('usuarios');
     
   }
 };
