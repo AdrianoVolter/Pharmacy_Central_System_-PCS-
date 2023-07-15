c5112093934ce523381a973dafa74702be86bf24
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-07-14 22:31:49 -0300

    feat: Criando funçao validar token

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -18,6 +18,19 @@ module.exports = {
         } catch (error) {
             return res.status(401).send({error: 'Não autorizado!'})
         }
-}
+    },
+
+// valilidar o token e decodiificar o token
+    async validarToken(req, res, next) {
+        try {
+            const { authorization } = req.headers;
+            const token = authorization.split(' ')[1];
+            const decode = verify(token, process.env.SECRET_KEY);
+            req.usuario = decode;
+            next();
+        } catch (error) {
+            return res.status(401).send({error: 'Não autorizado!'})
+        }
+    }
 }
 
