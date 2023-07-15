949bd217e4f4a6684819519086415c953212f71a
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-07-14 21:12:29 -0300

    feat: Criando metodo post depositos,rota

--- src/routes/DepositosRoutes.js
+++ src/routes/DepositosRoutes.js
@@ -2,3 +2,8 @@ const express = require('express');
 const routes = express.Router();
 const DepositoController = require('../controllers/DepositoController')
 const {auth} = require('../middleware/auth')
+
+
+routes.post('/depositos', auth , DepositoController.criarDeposito)
+
+module.exports = routes
