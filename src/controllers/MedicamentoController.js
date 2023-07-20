c9a30b1d605c3badcc9fba6386b8272bb4c95d5e
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-07-19 21:41:48 -0300

    feat: cadastro de medicamentos , validações cadastrar em um deposito especifico

--- src/controllers/MedicamentoController.js
+++ src/controllers/MedicamentoController.js
@@ -1,95 +1,104 @@
 const Medicamentos = require('../models/Medicamentos');
 const MedicamentosDepositos = require('../models/MedicamentosDepositos');
 const DepositoUsuarios = require('../models/DepositosUsuarios');
+const Depositos = require('../models/Depositos');
 
 module.exports = {
-  async cadastroMedicamento(req, res) {
-    try {
- 
-        const {
-            nome_medicamento,
-            nome_laboratorio,
-            descricao,
-            dosagem,
-            unidade_dosagem,
-            tipo_medicamento,
-            preco,
-            quantidade
-        } = req.body;
-        
-        const id_usuarios = req.usuario.id;
-        console.log(id_usuarios);
-        
-        const depositoUsuario = await DepositoUsuarios.findOne({
-            where: {
-            id_usuarios: id_usuarios
+    async cadastroMedicamento(req, res) {
+        try {
+            const {
+                nome_medicamento,
+                nome_laboratorio,
+                descricao,
+                dosagem,
+                unidade_dosagem,
+                tipo_medicamento,
+                preco,
+                quantidade,
+                id_depositos
+            } = req.body;
+    
+            const id_usuarios = req.usuario.id;
+            console.log(id_usuarios);
+    
+            const depositoUsuario = await DepositoUsuarios.findAll({
+                where: {
+                    id_usuarios: id_usuarios,
+                    id_depositos: id_depositos
+                }
+            });
+            //se digitou um depósito que não é do usuário
+            if (!depositoUsuario ||depositoUsuario.length == 0) {
+                return res.status(400).send({ error: 'Usuário não possui depósito associado!' });
             }
-        });
-        
-        if (!depositoUsuario) {
-            return res.status(400).send({ error: 'Usuário não possui depósito associado!' });
-        }
-        
-        const id_depositos = depositoUsuario.id_depositos; // Obtém o ID do depósito associado ao usuário
-        
-      
-        let medicamento = await Medicamentos.findOne({
-            where: {
-            nome_medicamento,
-            nome_laboratorio
+
+            const deposito = await Depositos.findOne({
+                where: {
+                    id: id_depositos
+                }
+            });
+    
+            console.log("Deposito encontrado:", deposito); // Adicionando o log
+    
+            if (!deposito) {
+                return res.status(400).send({ error: 'Depósito com o ID fornecido não existe!' });
             }
-        });
-        
-        if (medicamento) {
+    
+            let medicamento = await Medicamentos.findOne({
+                where: {
+                    nome_medicamento,
+                    nome_laboratorio
+                }
+            });
+    
+            if (!medicamento) {
+                medicamento = await Medicamentos.create({
+                    nome_medicamento,
+                    nome_laboratorio,
+                    descricao,
+                    dosagem,
+                    unidade_dosagem,
+                    tipo_medicamento
+                });
+            }
+    
             const medicamentoDeposito = await MedicamentosDepositos.findOne({
-            where: {
-                id_medicamentos: medicamento.id,
-                id_depositos: id_depositos
-            }
+                where: {
+                    id_medicamentos: medicamento.id,
+                    id_depositos: id_depositos
+                }
             });
-        
+    
             if (medicamentoDeposito) {
-            return res.status(400).send({ error: 'Medicamento já cadastrado neste depósito!' });
+                return res.status(400).send({ error: 'Medicamento já cadastrado neste depósito!' });
             }
-        }
-        
-        if (!medicamento) {
-            medicamento = await Medicamentos.create({
-            nome_medicamento,
-            nome_laboratorio,
-            descricao,
-            dosagem,
-            unidade_dosagem,
-            tipo_medicamento
+    
+            await MedicamentosDepositos.create({
+                id_medicamentos: medicamento.id,
+                id_depositos: id_depositos,
+                preco: preco,
+                quantidade: quantidade,
+                descricao: descricao
+            });
+    
+            return res.status(201).send({
+                identificador: medicamento.id,
+                nomeMedicamento: medicamento.nome_medicamento,
+                Message: "Medicamento criado e associado ao depósito!",
+                medicamento,
+                preco,
+                quantidade,
+                descricao
+            });
+    
+        } catch (err) {
+            
+            return res.status(500).send({
+                err: err.message,
+                cause: 'Erro no servidor!'
             });
         }
-        
-        await MedicamentosDepositos.create({
-            id_medicamentos: medicamento.id,
-            id_depositos: id_depositos,
-            preco: preco,
-            quantidade: quantidade,
-            descricao: descricao
-        });
-        
-        return res.status(201).send({
-            identificador: medicamento.id,
-            nomeMedicamento: medicamento.nome_medicamento,
-            Message: "Medicamento criado e associado ao depósito!",
-            medicamento,
-            preco,
-            quantidade,
-            descricao
-        });
-        
-    } catch (err) {
-      return res.status(500).send({
-       
-        err: err.message,
-        cause: 'Erro no servidor!'
-      });
-    }
-  },
+    }, 
 
     async atualizarMedicamento(req, res) {
         try {
